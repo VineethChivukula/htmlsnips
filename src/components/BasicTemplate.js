cca96747ab141301b3a6e65c8a4b637ab0dbe89d
Vineeth Chivukula <qualityhacker2002@gmail.com>
2024-02-02 21:07:07 +0530

    commit

--- src/components/BasicTemplate.js
+++ src/components/BasicTemplate.js
@@ -1,11 +1,26 @@
 import "../styles/basictemplate.css";
+import CodeMirror from '@uiw/react-codemirror';
 
-const Basics = () => {
+const BasicTemplate = () => {
     return (
         <div>
             <h1>The Basic HTML Template</h1>
+            <CodeMirror
+                width="30%"
+                // theme={"ayu-dark"}
+                value={`<!DOCTYPE html>
+<html>
+    <head>
+        <title>Basic HTML Template</title>
+    </head>
+    <body>
+        <h1>Hello, World!</h1>
+    </body>
+</html>`
+    }
+            />
         </div>
     );
 };
 
-export default Basics;
+export default BasicTemplate;
