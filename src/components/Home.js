9d166b6763204d1bea4dc9e3653f62ba1058c43b
Vineeth Chivukula <qualityhacker2002@gmail.com>
2024-02-02 19:45:23 +0530

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,10 @@ import "../styles/home.css";
 const Home = () => {
     return (
         <div className="home">
-            <h1>Welcome to my website!</h1>
-            <p>We hope you enjoy your visit.</p>
+            <h1>Welcome!</h1>
+            <p>My name is <b>Vineeth</b>, and I have made this webpage dedicated to revising important HTML tags.</p>
+            <p>Click on the top left icon to keep exploring.</p>
+            <p>Have a great day!</p>
         </div>
     );
 };
