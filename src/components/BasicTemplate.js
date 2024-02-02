125ccebbeedb366865edf15fe4ce6b1fa6643f6a
Vineeth Chivukula <qualityhacker2002@gmail.com>
2024-02-02 21:22:04 +0530

    commit

--- src/components/BasicTemplate.js
+++ src/components/BasicTemplate.js
@@ -1,13 +1,10 @@
 import "../styles/basictemplate.css";
-import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror from "@uiw/react-codemirror";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
 
-const BasicTemplate = () => {
-    return (
-        <div>
-            <h1>The Basic HTML Template</h1>
-            <CodeMirror
-                width="30%"
-                value={`<!DOCTYPE html>
+const code = `<!DOCTYPE html>
 <html>
     <head>
         <title>Basic HTML Template</title>
@@ -15,8 +12,23 @@ const BasicTemplate = () => {
     <body>
         <h1>Hello, World!</h1>
     </body>
-</html>`
-    }
+</html>`;
+
+const BasicTemplate = () => {
+    return (
+        <div>
+            <h1>The Basic HTML Template</h1>
+            <CodeMirror
+                width="50%"
+                height="50%"
+                value={code}
+                theme={tokyoNight}
+                extensions={[
+                    markdown({
+                        base: markdownLanguage,
+                        codeLanguages: languages,
+                    }),
+                ]}
             />
         </div>
     );
