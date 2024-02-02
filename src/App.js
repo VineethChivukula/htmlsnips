7e7267acd32437a4a8d75bf2bf4933fa5d70859a
Vineeth Chivukula <qualityhacker2002@gmail.com>
2024-02-02 15:03:49 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
 import { Divider } from "@mui/joy";
-
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
@@ -12,6 +16,7 @@ const App = () => {
                 <Divider />
                 <Routes>
                     <Route path="/Home" element={<Home />} />
+                    <Route path="/" element={<Navigate to="/Home" />} />
                     {/* other routes */}
                 </Routes>
             </div>
